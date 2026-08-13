c466f8a399f8d66147ea8473e472de2e7ccdf4dc
Adacska-JJK <buktadani.bd@gmail.com>
2026-08-13 12:48:03 +0200

    Fixing PaletteControll

--- Assets/Scripts/PaletteController.cs
+++ Assets/Scripts/PaletteController.cs
@@ -42,61 +42,73 @@ public class PaletteController : MonoBehaviour
     {
         SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
 
-        if (currentState == State.MovingOnPath)
+        switch (currentState)
         {
-            if (wayPointIndex < wayPoints.Length)
-            {
-                Vector3 target = wayPoints[wayPointIndex].position;
-                transform.position = Vector3.MoveTowards(transform.position, target, movingSpeed * Time.deltaTime);
-
-                if (Vector3.Distance(transform.position, target) < 0.05f)
-                {
-                    transform.position = target;
-
-                    if (wayPointIndex == SearchIndex(wayPoints, simController.elagazasEleje))
-                    {
-                        activeBranch = (spriteRenderer.color == Color.green) ? simController.elagazasA : simController.elagazasB;
-                        branchIndex = 0;
-                        currentState = State.InBranch;
-                    }
-                    else
-                    {
-                        wayPointIndex++;
-                        if (wayPointIndex >= wayPoints.Length)
-                        {
-                            wayPointIndex = 0;
-                        }
-                    }
-
-                }
-            }
-
+            case State.MovingOnPath:
+                MovingOnPath(spriteRenderer);
+                break;
+            case State.InBranch:
+                InBranch();
+                break;
         }
-        else if (currentState == State.InBranch)
-        {
-            if (branchIndex < activeBranch.Length)
-            {
-                Vector3 target = activeBranch[branchIndex].position;
-                transform.position = Vector3.MoveTowards(transform.position, target, movingSpeed * Time.deltaTime);
 
-                if (Vector3.Distance(transform.position, target) < 0.05f)
-                {
-                    transform.position = target;
-                    branchIndex++;
-                }
-            }
-            else
+    }
+    private void MovingOnPath(SpriteRenderer spriteRenderer)
+    {
+        if (wayPointIndex < wayPoints.Length)
+        {
+            Vector3 target = wayPoints[wayPointIndex].position;
+            transform.position = Vector3.MoveTowards(transform.position, target, movingSpeed * Time.deltaTime);
+
+            if (Vector3.Distance(transform.position, target) < 0.05f)
             {
-                transform.position = Vector3.MoveTowards(transform.position, simController.elagazasVege.position, movingSpeed * Time.deltaTime);
-                if (Vector3.Distance(transform.position, simController.elagazasVege.position) < 0.05f)
+                transform.position = target;
+
+                if (wayPointIndex == SearchIndex(wayPoints, simController.elagazasEleje))
                 {
-                    transform.position = simController.elagazasVege.position;
-                    currentState = State.MovingOnPath;
-                    wayPointIndex = SearchIndex(wayPoints, simController.elagazasVege);
+                    activeBranch = (spriteRenderer.color == Color.green) ? simController.elagazasA : simController.elagazasB;
+                    branchIndex = 0;
+                    currentState = State.InBranch;
                 }
+                else
+                {
+                    wayPointIndex++;
+                    if (wayPointIndex >= wayPoints.Length)
+                    {
+                        wayPointIndex = 0;
+                    }
+                }
+
             }
         }
     }
+    private void InBranch()
+    {
+
+        if (branchIndex < activeBranch.Length)
+        {
+            Vector3 target = activeBranch[branchIndex].position;
+            transform.position = Vector3.MoveTowards(transform.position, target, movingSpeed * Time.deltaTime);
+
+            if (Vector3.Distance(transform.position, target) < 0.05f)
+            {
+                transform.position = target;
+                branchIndex++;
+            }
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, simController.elagazasVege.position, movingSpeed * Time.deltaTime);
+            if (Vector3.Distance(transform.position, simController.elagazasVege.position) < 0.05f)
+            {
+                transform.position = simController.elagazasVege.position;
+                currentState = State.MovingOnPath;
+                wayPointIndex = SearchIndex(wayPoints, simController.elagazasVege);
+            }
+        }
+    }
+
+
     private int SearchIndex(Transform[] elagazas, Transform target)
     {
         for (int i = 0; i < elagazas.Length; i++)
